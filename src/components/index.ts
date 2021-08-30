a7d5d4c5cc32db397ce71c50be65c2f3ac487139
bfgonzalez <bfgonzalez142@gmail.com>
2021-08-30 15:54:30 +0800

    Fixes import error

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,9 @@
-import Layout from './Shared/Layout';
-import Section from './Shared/Section';
+import Layout from './shared/Layout/index';
+import Section from './shared/Section';
 import Hero from './Hero';
 import About from './About';
 import Tech from './Tech';
 import SkillSection from './Tech/SkillSection';
-import { Header, Paragraph, LinkText } from './Shared/Typography';
+import { Header, Paragraph, LinkText } from './shared/Typography';
 
 export { Layout, Section, Header, Paragraph, LinkText, SkillSection, Hero, About, Tech }
