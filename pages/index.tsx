eb42e40efd176aa88962c0033a8d2bd9430d7d78
bfgonzalez <bfgonzalez142@gmail.com>
2021-08-30 10:53:02 +0800

    Moves footer to Layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,6 @@ const Home: React.FC = () => {
   return (
     <Layout>
       <Hero />
-      <footer className='font-secondary py-8 text-center'>
-        <a
-          href='https://bfgonzalez.vercel.app/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Bianca Gonzalez © {new Date().getFullYear()}
-        </a>
-      </footer>
     </Layout>
   )
 }
