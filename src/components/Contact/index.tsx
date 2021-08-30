fa523d6f613164509c237b8b1df394beb232bee3
bfgonzalez <bfgonzalez142@gmail.com>
2021-08-30 17:47:00 +0800

    Updates website thumbnail

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -3,25 +3,23 @@ import { Section, Header, Paragraph, LinkText } from '..'
 const Contact: React.FC = () => (
   <Section>
     <Header text="Let's Connect 👋" />
-    <Paragraph>
-      <div className='flex -ml-3'>
-        <LinkText
-          link='https://github.com/bfgonzalez'
-          text='Github'
-          color='black'
-        />
-        <LinkText
-          link='https://www.linkedin.com/in/bfgonzalez/'
-          text='LinkedIn'
-          color='black'
-        />
-        <LinkText
-          link='https://www.instagram.com/biancapture/'
-          text='Instagram'
-          color='black'
-        />
-      </div>
-    </Paragraph>
+    <div className='flex -ml-3 font-secondary text-xl lg:text-2xl my-6'>
+      <LinkText
+        link='https://github.com/bfgonzalez'
+        text='Github'
+        color='black'
+      />
+      <LinkText
+        link='https://www.linkedin.com/in/bfgonzalez/'
+        text='LinkedIn'
+        color='black'
+      />
+      <LinkText
+        link='https://www.instagram.com/biancapture/'
+        text='Instagram'
+        color='black'
+      />
+    </div>
   </Section>
 )
 
