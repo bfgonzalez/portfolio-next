ecc9ec61f9a49ee9f0007cac30c3cf647bbd84eb
bfgonzalez <bfgonzalez142@gmail.com>
2021-08-30 10:52:12 +0800

    Moves hero content to separate component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,19 @@
 import Layout from '../src/components/shared/Layout'
+import Hero from '../src/components/Hero'
 
 const Home: React.FC = () => {
   return (
     <Layout>
-      <div className='h-screen flex flex-col justify-center items-center px-2'>
-        <main className='flex-1 flex flex-col justify-center items-start py-20 text-left w-3/4'>
-          <h1
-            className='font-primary font-bold hover:underline m-0 text-6xl'
-            style={{ color: '#0070f3' }}
-          >
-            <a href='https://github.com/bfgonzalez'>Software Developer</a>
-          </h1>
-
-          <p className='font-secondary text-2xl mt-4'>
-            Hey there, I'm Bianca! I solve problems and bring ideas to life
-            through tech!
-          </p>
-        </main>
-
-        <footer className='font-secondary py-8'>
-          <a
-            href='https://bfgonzalez.vercel.app/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Bianca Gonzalez © {new Date().getFullYear()}
-          </a>
-        </footer>
-      </div>
+      <Hero />
+      <footer className='font-secondary py-8 text-center'>
+        <a
+          href='https://bfgonzalez.vercel.app/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Bianca Gonzalez © {new Date().getFullYear()}
+        </a>
+      </footer>
     </Layout>
   )
 }
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,21 @@
+const Hero: React.FC = () => {
+  return (
+    <div className='h-screen flex flex-col justify-center items-center px-2'>
+      <main className='flex-1 flex flex-col justify-center items-start py-20 text-left w-3/4'>
+        <h1
+          className='font-primary font-bold hover:underline m-0 text-6xl'
+          style={{ color: '#0070f3' }}
+        >
+          <a href='https://github.com/bfgonzalez'>Software Developer</a>
+        </h1>
+
+        <p className='font-secondary text-2xl mt-4'>
+          Hey there, I'm Bianca! I solve problems and bring ideas to life
+          through tech!
+        </p>
+      </main>
+    </div>
+  )
+}
+
+export default Hero
