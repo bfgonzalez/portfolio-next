28952b39c109a655275f66c68dd1791c1aec2ed9
bfgonzalez <bfgonzalez142@gmail.com>
2021-08-30 10:41:12 +0800

    Adds thumbnail image via meta tags

--- src/components/shared/Layout/index.tsx
+++ src/components/shared/Layout/index.tsx
@@ -9,6 +9,7 @@ const Layout: React.FC = ({ children }) => {
           name='description'
           content="Hey there, I'm Bianca! I solve problems and bring ideas to life through tech!"
         />
+        <meta property='og:image' content='/thumbnail.png' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       {children}
